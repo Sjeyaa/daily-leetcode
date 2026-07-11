8d50743688b9bdc7375629adf72fbe9168c594b4
Sjeyaa <110975036+Sjeyaa@users.noreply.github.com>
2026-07-11 16:16:47 +0530

    Time: 45 ms (25.86%) | Memory: 47.1 MB (39.73%) - LeetSync

---
+++ TwoSum/1-two-sum/two-sum.java
@@ -0,0 +1,18 @@
+class Solution 
+{
+    public int[] twoSum(int[] arr, int x) 
+    {
+        for(int i=0;i<arr.length-1;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[i]+arr[j]==x)
+            {
+                return new int[]{i,j};
+            }
+            }
+            
+        }
+        return new int[]{0};
+    }
+}
